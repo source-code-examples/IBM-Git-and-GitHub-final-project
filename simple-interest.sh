74e2aa1831ef95379af6b431dbaaa45b1e259067
source-code-examples <118019715+source-code-examples@users.noreply.github.com>
2024-09-17 00:45:41 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,7 @@
+let principalAmountinEuro = 10000;
+let timePeriodInYears = 5;
+let interestRateAsDecimal = 0.2;
 
+const simpleInterest = (principalAmountinEuro * timePeriodInYears * interestRateAsDecimal) / 100;
+
+console.log(`Simple Interest: ${simpleInterest}`);
